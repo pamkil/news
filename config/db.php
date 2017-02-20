68cf6af17509707942b358a4eac7d9dde9c950b7
pamkil <pamkil@yandex.ru>
2017-02-20 14:30:36 +0000

    init

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=mobile',
+    'username' => 'mobile',
+    'password' => 'mobile',
+    'charset' => 'utf8',
+];
